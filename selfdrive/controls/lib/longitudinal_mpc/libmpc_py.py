44aa63753fd1fcaf04a1e8daa918d40a66b56113
Luke Adams <luke.adams@belljar.io>
2021-01-04 04:49:18 +0000

    longitudinal_mpc: build with cmake

--- selfdrive/controls/lib/longitudinal_mpc/libmpc_py.py
+++ selfdrive/controls/lib/longitudinal_mpc/libmpc_py.py
@@ -1,9 +1,9 @@
 import os
-
+from common.basedir import BASEDIR
 from cffi import FFI
 from common.ffi_wrapper import suffix
 
-mpc_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)))
+mpc_dir = os.path.join(BASEDIR, "build/selfdrive/controls/lib/longitudinal_mpc")
 
 def _get_libmpc(mpc_id):
     libmpc_fn = os.path.join(mpc_dir, "libmpc%d%s" % (mpc_id, suffix()))
